e9d7eb0123278df951d0a1a496276c201578434b
doanlecong <doanlecong1811@gmail.com>
2017-04-06 21:35:54 +0700

    Thêm từ Vũ Duy Trúc

---
+++ app/Lo_nhuom.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lo_nhuom extends Model
+{
+    //
+}
---
+++ app/Phieu_xuat_moc.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Phieu_xuat_moc extends Model
+{
+    //
+}
