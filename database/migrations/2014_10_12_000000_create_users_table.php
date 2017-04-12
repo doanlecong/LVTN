171a9a049bffd0b27775d3d7316ee2d692f7934b
doanlecong <doanlecong1811@gmail.com>
2017-04-13 02:48:33 +0700

    Chính lại tên file ấy mà

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,21 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->timestamps();
-
             $table->rememberToken();
-
-            $table->string('ten_dang_nhap');
-            $table->string('chuc_vu');
-            $table->integer('quyen');
-            $table->integer('luong');
-            $table->text('ghi_chu');
-            $table->date('ngay_sinh');
-            $table->string('gioi_tinh', 1);
-            $table->string('dia_chi');
-            $table->string('so_dien_thoai', 15);
-
-            $table->tinyInteger('da_xoa');
+            $table->timestamps();
         });
     }
 
