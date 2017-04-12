3d380c79dc1e18e3d3d820d00d6a9596e290fac2
doanlecong <doanlecong1811@gmail.com>
2017-04-13 02:45:34 +0700

    Thêm một số đường link

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,28 @@
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('manageside.kho.manager_kho');
+});
+// Route::get('manage_kho_loai_soi',function(){
+    
+//     return view('manageside.manager_kho_loai_soi');
+
+// });
+//Các Route để quản lý ben kho cua Admin , chưa hiện thực authenication cho admin
+Route::resource('/manage_kho_loai_soi','LoaiSoiController');
+Route::resource('/manage_kho_nha_cung_cap','NhaCungCapController');
+Route::resource('/manage_kho_don_hang_cong_ty','DonHangCongTyController');
+Route::resource('/manage_kho_hoa_don_nhap','HoaDonNhapController');
+Route::resource('/manage_kho_phieu_xuat_soi','PhieuXuatSoiController');
+
+//Admin end
+Route::get('/manage_san_xuat', function(){
+    return view('manageside.manager_san_xuat');
+});
+
+Route::get('/manage_ban_hang',function(){
+    return view('manageside.manager_ban_hang');
+});
+Route::get('/manage_thong_ke',function (){
+    return view('manageside.manager_thong_ke');
 });
