874f5d31d5db1556404344b0c8d3c29e5f266827
Truc <truc193204@gmail.com>
2017-05-16 16:22:37 +0700

    fix database migrate

--- src/database/migrations/2017_04_16_022029_add_mau_id_vao_cay_vai_thanh_pham_table.php
+++ src/database/migrations/2017_04_16_022029_add_mau_id_vao_cay_vai_thanh_pham_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddMauVaoCayVaiThanhPhamTable extends Migration
+class AddMauIdVaoCayVaiThanhPhamTable extends Migration
 {
     /**
      * Run the migrations.
